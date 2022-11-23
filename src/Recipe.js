da3517921e42ac32030eca4171c2ca5917c85bfd
Vipul <vipulbarjatya@gmail.com>
2022-11-23 12:50:06 +0530

    components and props

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,7 +1,30 @@
 import React from "react";
 
-function recipe() {
-  return <> Hello </>;
+function recipe(props) {
+  const { name, cookTime, servings, instructions } = props;
+  return (
+    <>
+      <div>
+        <h3>{name}</h3>
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
+      <div>
+        <span>Cook Time :</span>
+        <span>{cookTime}</span>
+      </div>
+      <div>
+        <span>Servings :</span>
+        <span>{servings}</span>
+      </div>
+      <div>
+        <span>Instructions:</span>
+        <div>{instructions}</div>
+      </div>
+    </>
+  );
 }
 
 export default recipe;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Recipe from "./Recipe";
 
-function RecipeList() {
+function RecipeList({ recipes }) {
   return (
-    <>
-      <Recipe></Recipe>
-      <Recipe></Recipe>
-    </>
+    <div>
+      {recipes.map((recipe) => {
+        return <Recipe key={recipe.id} {...recipe} />;
+      })}
+    </div>
   );
 }
 
